0b42b290cffdfe98d3c2c7e798ff19806c639545
Pratik Chaubey <pratikup98@gmail.com>
2025-08-16 23:08:41 +0530

    Tailwind

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -7,18 +7,16 @@ function Card({ task, settask }) {
   };
 
   return (
-    <div className="w-80 max-w-full bg-white min-h-screen rounded-3xl shadow-2xl p-8 flex flex-colgit push
- justify-center items-center relative hover:scale-105 hover:shadow-3xl transform transition-transform duration-300 ease-in-out"
->
+    <div className="w-80 max-w-full bg-white min-h-screen rounded-3xl shadow-xl p-8 flex flex-col justify-center items-center relative hover:scale-105 hover:shadow-2xl transform transition duration-300 ease-in-out">
       <div className="flex flex-wrap justify-center gap-8">
         {task.length > 0 ? (
           task.map((taskData) => (
             <div
               key={taskData.id}
-              className="w-70 bg-blend-color rounded-2xl shadow-2xl p-6 relative hover:scale-105 transform transition-all duration-300"
+              className="w-72 bg-white rounded-2xl shadow-xl p-6 relative hover:scale-105 transition duration-300"
             >
               <h1 className="text-center text-2xl font-extrabold text-red-700 mb-3 tracking-wide">
-                 
+                Note
               </h1>
               <p className="text-lg text-gray-700 font-medium leading-relaxed break-words">
                 {taskData.Write}
